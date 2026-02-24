63e2d1b1f9836ffb528b3950e25ee36695ac3374
AhmedMahmoud-0 <ahmdabwhykl11@gmail.com>
2026-02-24 18:44:12 +0200

    second commit

--- app/-components/Header.jsx
+++ app/-components/Header.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+"use client";
+import React, { use, useEffect, useState } from "react";
 import Image from "next/image";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import Api from "../-utils/Api";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 const Header = () => {
+  const [Category, setCategory] = useState([]);
+
+  useEffect(() => {
+    getCategorylist();
+  }, []);
+
+  const getCategorylist = () => {
+    Api.getCategory().then((resp) => {
+      setCategory(resp.data.data);
+    });
+  };
   return (
     <div className="shadow-md flex justify-between p-2">
       <div className="flex items-center gap-8">
-        <Image width={100} height={100} src="/logo.png.png" alt="" />
-        <h2
-          className="flex gap-2 items-center
+        <Image width={100} height={100} src="/logo.png.png" alt="" priority />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="flex gap-2 items-center
           border rounded-full p-2 bg-slate-200"
-        >
-          <LayoutGrid className="h-5 w-5" />
-          Category
-        </h2>
+            >
+              <LayoutGrid className="h-5 w-5" />
+              Category
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+            {Category.map((cat) => (
+              <DropdownMenuItem key={cat.id}>{cat.name}</DropdownMenuItem>
+
+            ))}
+
+
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <div className="hidden md:flex items-center gap-3 border rounded-full p-2 ">
           <Search />
           <input type="text" className="outline-none" placeholder="search" />
